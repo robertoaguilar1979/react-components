94f3ca350b3be6764e78f573003ba6516c3671cb
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-05-24 23:34:37 -0700

    added useState and useEffect to project

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const url = "https://api.github.com/users";
   return (
     <div className="App">
       <h1>hello world</h1>
