350bf93f427b9bff60da740ec91670dd30f6a763
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-05-25 22:34:56 -0700

    added async and await to get url

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
+const url = "https://api.github.com/users";
 
 function App() {
-  const url = "https://api.github.com/users";
+  const [users, setUsers] = useState([]);
+
+  let getUsers = async () => {
+    let response = await fetch(url);
+    let users = await response.json();
+    console.log(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
       <h1>hello world</h1>
