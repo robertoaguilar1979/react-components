fa281f600991b3259dbd2fc96abb96584f85a818
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-05-30 20:58:12 -0700

    added useEffect and fetched data

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,41 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      let response = await fetch(url);
+      let items = await response.json();
+      setLoading(false);
+      setTours(items);
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="App">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Tours />
     </div>
   );
 }
