73db548b30aceddbbdcffd1c7b5c806d6dacb1a0
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-05-30 17:43:47 -0700

    started new tours component folder

---
+++ tours/components/Tour.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Tour = () => {
+  return (
+    <div>
+      <h2>tour component</h2>
+    </div>
+  );
+};
+
+export default Tour;
---
+++ tours/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
