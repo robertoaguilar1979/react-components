59c840882ffc57bac77133c68664dd5e16dbaf37
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-05-31 19:17:49 -0700

    added toggle button to paragraph information in component

--- tours/src/components/Tour.js
+++ tours/src/components/Tour.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Tour = ({ image, info, name, price }) => {
+  const [readmore, setReadmore] = useState(false);
+
   return (
     <div className="item">
       <img src={image} alt={name} />
@@ -8,7 +10,13 @@ const Tour = ({ image, info, name, price }) => {
         <h2>{name}</h2>
         <p>${price}</p>
         <div>
-          <p>{info}</p>
+          <p>
+            {readmore ? info : `${info.substring(0, 200)}`}
+
+            <button className="show" onClick={() => setReadmore(!readmore)}>
+              {readmore ? "show less" : "show more"}
+            </button>
+          </p>
           <button>not interested</button>
         </div>
       </div>
