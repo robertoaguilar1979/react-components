d4a2c079da9bdd051ec338ad6293a8230c808f1a
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-01 19:01:21 -0700

    added data and cleaned up react

---
+++ reviews/src/components/Reviews.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import people from "../data/data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  return (
+    <>
+      <h2>review component</h2>
+      <FaChevronLeft /> <FaChevronRight />
+    </>
+  );
+};
+
+export default Review;
