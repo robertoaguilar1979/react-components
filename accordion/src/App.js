a99b3375652e154c2610deccb0a1bc315943e4f5
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-08 13:21:14 -0700

    finished accordion component and css

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import data from "./data/data";
 import Questions from "./components/Questions";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <div className="App">
-      <h1>hello accordion</h1>
-      <Questions />
+      <h1>Accordion components</h1>
+      {questions.map((data) => {
+        return <Questions key={data.id} {...data} />;
+      })}
     </div>
   );
 }
--- accordion/src/components/Questions.js
+++ accordion/src/components/Questions.js
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
-function Questions() {
-  return <div>Questions</div>;
+function Questions({ title, info }) {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div className="container">
+      <div className="question">
+        <h2>{title}</h2>
+        <button
+          className={showInfo ? "red" : "green"}
+          onClick={() => setShowInfo(!showInfo)}
+        >
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </div>
+
+      {showInfo && <p>{info}</p>}
+    </div>
+  );
 }
 
 export default Questions;
