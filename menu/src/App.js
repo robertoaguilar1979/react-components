b025dcf900bc190b29107bab66032966e0a7c51f
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-08 18:12:39 -0700

    display items in menu

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import data from "./data/data";
+import Data from "./data/data";
+import React, { useState, useEffect } from "react";
+import Menu from "./components/Menu";
 
 function App() {
+  const [data, setdata] = useState(Data);
+  const [search, setSearch] = useState([]);
+
   return (
     <div className="App">
       <h1>menu component</h1>
+      <Menu items={data} />
     </div>
   );
 }
--- menu/src/components/Menu.js
+++ menu/src/components/Menu.js
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-function Menu() {
-  return <div>Menu</div>;
+function Menu({ items }) {
+  return (
+    <div>
+      {items.map((item) => {
+        const { id, title, category, price, img, desc } = item;
+        return (
+          <div key={id}>
+            <img src={img} alt={title} />
+            <div>
+              <h2>{title}</h2>
+              <p>{price}</p>
+              <p>{desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Menu;
