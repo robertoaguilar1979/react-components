01776e81f5cbe944c01c6dafe746aeb547ab337e
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-13 16:02:49 -0700

    preparing tabs app cleaned css and added npm i react-icons

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Tabs from "./components/Tabs";
 
 function App() {
   return (
     <div className="App">
       <h1>tabs components</h1>
+      <Tabs />
     </div>
   );
 }
---
+++ tabs/src/components/Tabs.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+let url = "https://course-api.com/react-tabs-project";
+
+function Tabs() {
+  return <div>Tabs</div>;
+}
+
+export default Tabs;
