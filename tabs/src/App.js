1414a41999aaf48987f0c9beff4b78e0dd71cd11
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-22 23:25:45 -0700

    added new state to tabs component

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect
+} from "react";
 import Tabs from "./components/Tabs";
+let url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>tabs components</h1>
-      <Tabs />
-    </div>
+
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+
+
+  return ( <
+    div className = "App" >
+    <
+    h1 > tabs components < /h1> <
+    Tabs / >
+    <
+    /div>
   );
 }
 
--- tabs/src/components/Tabs.js
+++ tabs/src/components/Tabs.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
-let url = "https://course-api.com/react-tabs-project";
 
 function Tabs() {
   return <div>Tabs</div>;
