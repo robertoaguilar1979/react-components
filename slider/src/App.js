cde2f58914d9d56499b3dd0346e08effb778a076
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-09 19:02:53 -0700

    added new data folder and file

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
 
 function App() {
   return (
