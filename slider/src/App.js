0c9a1740daeb62718567aec51a18137221c22678
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-10 23:34:08 -0700

    added new data import to App.js file

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
+import data from "..data/data";
 
 function App() {
   return (
