03c9afa39a6bc7cee4105750896f20c306bf214b
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-12 22:16:20 -0700

    added new states in App.js file

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,9 +2,13 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
-import data from "..data/data";
+import data from "./data/data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  console.log(people);
   return (
     <div className="App">
       <h1> slider </h1>{" "}
